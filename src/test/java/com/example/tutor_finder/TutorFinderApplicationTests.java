388a130e9bfa7938ac3efd1f2d566b3e47fbc067
Achal Shah <shahachal1711@gmail.com>
2022-07-18 15:59:53 +0530

    Spring boot dir

---
+++ src/test/java/com/example/tutor_finder/TutorFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tutor_finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
